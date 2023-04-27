425699959c880aa31e4b8b75660f672b356bb236
r.selvakumar <r.selvakumar@stackroute.in>
2023-04-27 16:58:50 +0530

    initial commit

---
+++ src/main/java/com/example/springbootjenkindemo1/SpringbootJenkinDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkindemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjenkindemo1/SpringbootJenkinDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkindemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
